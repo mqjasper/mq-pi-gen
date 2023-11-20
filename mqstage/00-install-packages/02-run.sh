dfac7067eba137972807825989beb61eca1ea8f8
mqjasper <jasper.catley@students.mq.edu.au>
2023-11-20 15:16:52 +1100

    Updates to package installations, SN echo, etc

--- mqstage/00-install-packages/02-run.sh
+++ mqstage/00-install-packages/02-run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 on_chroot << EOF
-python3 -m pip install --upgrade pip
-python3 -m pip install 'https://www.piwheels.org/simple/numpy/numpy-1.25.2-cp39-cp39-linux_armv7l.whl'
+mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/GRR
+#python3 -m pip install --upgrade pip
+python3 -m pip install 'https://www.piwheels.org/simple/numpy/numpy-1.26.1-cp311-cp311-linux_armv7l.whl'
 pip install tflite-runtime
 pip install Pillow
+mv /usr/lib/python3.11/GRR /usr/lib/python3.11/EXTERNALLY-MANAGED 
 EOF
--- mqstage/01-ap-config/00-run.sh
+++ mqstage/01-ap-config/00-run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
 # Clone the latest version of the stem_club repository
 cd files/ && rm -r stem_club && git clone https://github.com/altmattr/stem_club.git
+cd stem_club && git checkout picamera2_conversion && cd ..
 cd ..
 sudo cp -a files/stem_club "${ROOTFS_DIR}/home/pi/stem_club/"
 # Install all the services included in the stem_club repository. Only the interface is enabled from boot. 
 install -m 644 "${ROOTFS_DIR}/home/pi/stem_club/services/interface.service" "${ROOTFS_DIR}/lib/systemd/system/"
 install -m 644 "${ROOTFS_DIR}/home/pi/stem_club/services/prediction.service" "${ROOTFS_DIR}/lib/systemd/system/"
 install -m 644 "${ROOTFS_DIR}/home/pi/stem_club/services/sensors.service" "${ROOTFS_DIR}/lib/systemd/system/"
+# Install wheel for legacy picamera package
+install -m 777 files/*.whl "${ROOTFS_DIR}/home/pi"
+install -m 777 files/install-picamera "${ROOTFS_DIR}/etc/init.d/"
 # Copy preconfigured NetworkManager access point config to directory
 install -m 600 files/*.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
 # Load the first-boot service to randomise the SSID
@@ -14,6 +18,8 @@ install -m 600 files/*.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-con
 #install -m 777 files/first-boot-rename "${ROOTFS_DIR}/etc/init.d/"
 # Load the service to rename the SSID on boot
 install -m 777 files/rename-on-boot "${ROOTFS_DIR}/etc/init.d/"
+# Install a basic script that echoes the pi's serial number to the console, for use when accessing rsync server
+install -m 777 files/serial-number "${ROOTFS_DIR}/usr/bin"
 # Custom MOTD when logging in. Will provide IP address, shui status, system uptime, journalctl command
 install -m 777 files/motd "${ROOTFS_DIR}/home/pi/"
 # Using the code from raspi-config to enable NetworkManager, also sense hat, first-boot program, custom MOTD
@@ -24,9 +30,6 @@ install -m 777 files/motd "${ROOTFS_DIR}/home/pi/"
 on_chroot << EOF
 update-rc.d rename-on-boot defaults
 systemctl enable interface.service
-systemctl -q stop dhcpcd 2> /dev/null
-systemctl -q disable dhcpcd
-systemctl -q enable NetworkManager
 cat /home/pi/motd | tee --append /home/pi/.bashrc > /dev/null
 rm /home/pi/motd
 sudo chmod -R 777 /home/pi/

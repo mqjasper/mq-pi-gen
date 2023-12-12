2ebf0444e83b45eda701c53e05ac673405ea1ffb
mqjasper <jasper.catley@students.mq.edu.au>
2023-12-12 18:26:33 +1100

    fix for the wps fix

--- exstage/02-netcon/00-run.sh
+++ exstage/02-netcon/00-run.sh
@@ -12,5 +12,6 @@ on_chroot << EOF
 update-rc.d rename-on-boot defaults
 cat /home/pi/motd | tee --append /home/pi/.bashrc > /dev/null
 rm /home/pi/motd
-wpa_cli wps_ap_pin disable
+echo -e "wpa_cli wps_ap_pin disable" | tee -a /home/pi/.bashrc
+echo -e "sed -i '/wpa_cli/d' /home/pi/.bashrc" | tee -a /home/pi/.bashrc
 EOF
